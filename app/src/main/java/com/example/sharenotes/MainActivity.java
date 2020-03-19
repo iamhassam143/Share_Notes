90c31ac1784013a4fafcb9b767c723d503832a92
anees ahmed siddiqui <james2420bond@gmail.com>
2020-03-19 18:26:11 +0530

    First Version It include splash  screen only

---
+++ app/src/main/java/com/example/sharenotes/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.sharenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
+
+    //animation
+      View first,second,third,fourth,fifth,sixth;
+    ImageView a;
+    //Animations
+    Animation topAnimantion,middleAnimation;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+
+        setContentView(R.layout.activity_main);
+        //this will bind your MainActivity.class file with activity_main.
+
+
+
+
+
+        first = findViewById(R.id.first_line);
+        second = findViewById(R.id.second_line);
+        third = findViewById(R.id.third_line);
+        fourth = findViewById(R.id.fourth_line);
+        fifth = findViewById(R.id.fifth_line);
+        sixth = findViewById(R.id.sixth_line);
+        a = findViewById(R.id.Mainlogo);
+
+
+//        Animation initialize
+        topAnimantion = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        middleAnimation = AnimationUtils.loadAnimation(this, R.anim.middle_anim);
+
+
+
+        first.setAnimation(topAnimantion);
+        second.setAnimation(topAnimantion);
+        third.setAnimation(topAnimantion);
+        fourth.setAnimation(topAnimantion);
+        fifth.setAnimation(topAnimantion);
+        sixth.setAnimation(topAnimantion);
+        a.setAnimation(middleAnimation);
+
+
+
+            new Handler().postDelayed(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+
+                    Intent i=new Intent(MainActivity.this,
+                            LoginActivity.class);
+                    //Intent is used to switch from one activity to another.
+
+                    startActivity(i);
+                    //invoke the SecondActivity.
+
+                    finish();
+                    //the current activity will get finished.
+                }
+            }, SPLASH_SCREEN_TIME_OUT);
+        }
+
+}
+
